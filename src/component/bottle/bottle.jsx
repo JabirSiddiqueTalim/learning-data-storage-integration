43d5cb07437ba2129df820898780043950bf612a
JabirSiddiqueTalim <jsiddiquetalim@gmail.com>
2026-03-03 00:10:28 +0600

    added grid and image bottles

--- src/component/bottle/bottle.jsx
+++ src/component/bottle/bottle.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
+import './bottle.css';
 
 const Bottle = (bottle) => {
   console.log(bottle)
   return (
     <div>
-      <h3>Bottle : {bottle.bottle.id}</h3>
+     
+
+      <div className='card '>
+        <h3>Bottle : {bottle.bottle.id}</h3>
+        <img className="bottle-img"src={bottle.bottle.image} alt="" />
+
+      </div>
 
     </div>
+
   );
 };
 
--- src/component/bottles/Bottles.jsx
+++ src/component/bottles/Bottles.jsx
@@ -1,16 +1,23 @@
 import React, { use } from 'react';
 import Bottle from '../bottle/bottle';
-// 
+import './bottles.css';
 const Bottles = ({bottlePromise}) => {
   // console.log(bottlePromise);
   const bottles=use(bottlePromise);
   // console.log(bottles);
   return (
-    <div>
-      {
+    <div className=''>
+       <h2>Bottle Show </h2>
+       <div className='grid-contrainer'>
+       {
+        
+
         bottles.map(bottle => <Bottle bottle={bottle}></Bottle>)
       }
 
+
+       </div>
+      
       
     </div>
   );

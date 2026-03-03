75ef11bc9bae1c06e269d60c5d3b7e3e0daf6238
JabirSiddiqueTalim <jsiddiquetalim@gmail.com>
2026-03-04 00:55:53 +0600

    update the logic

--- src/component/bottles/Bottles.jsx
+++ src/component/bottles/Bottles.jsx
@@ -1,35 +1,32 @@
 import React, { use, useState } from 'react';
 import Bottle from '../bottle/Bottle';
 import './bottles.css';
-const Bottles = ({bottlePromise}) => {
+const Bottles = ({ bottlePromise }) => {
   // console.log(bottlePromise);
-  const bottles=use(bottlePromise);
+  const bottles = use(bottlePromise);
   // console.log(bottles);
-  const [selectedbuy,setSelected]=useState([]);
-  const handleBottle=(bottle)=>
-  {
-    const newSelected=[...selectedbuy,bottle];
+  const [selectedbuy, setSelected] = useState([]);
+  const handleBottle = (bottle) => {
 
-    setSelected(newSelected);
-    // console.log(selected);
-    
-  }
+    const itemSelected = selectedbuy.find(item => item.id === bottle.id);
+  
+    if (!itemSelected) {
+      const newSelected = [...selectedbuy, bottle];
+      setSelected(newSelected);
+    } else {
+      const newSelected = selectedbuy.filter(item => item.id !== bottle.id);
+      setSelected(newSelected);
+    }
+  };
   return (
     <div className=''>
-       
-       <h2>Bottle Show </h2>
-       <h2>Number of bottle select : {selectedbuy.length} </h2>
-       <div className='grid-contrainer'>
-       {
-        
-
-        bottles.map(bottle => <Bottle key={bottle.id}  handleBottle={ ()=>handleBottle(bottle)} bottle={bottle}></Bottle>)
-      }
-
-
-       </div>
-      
-      
+      <h2>Bottle Show </h2>
+      <h2>Number of bottle select : {selectedbuy.length} </h2>
+      <div className='grid-contrainer'>
+        {
+          bottles.map(bottle => <Bottle key={bottle.id} handleBottle={() => handleBottle(bottle)} bottle={bottle}></Bottle>)
+        }
+      </div>
     </div>
   );
 };

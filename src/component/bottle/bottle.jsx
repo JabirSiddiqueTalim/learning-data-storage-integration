344710818d539c306f3533748b1437ebfac8bffa
JabirSiddiqueTalim <jsiddiquetalim@gmail.com>
2026-03-04 00:27:19 +0600

    added add and drop bottle

--- src/component/bottle/bottle.jsx
+++ src/component/bottle/bottle.jsx
@@ -1,19 +1,33 @@
-import React from 'react';
+import React, {useState } from 'react';
 import './bottle.css';
 
-const Bottle = (bottle) => {
-  console.log(bottle)
+const Bottle = ({bottle, handleBottle}) => {
+  console.log(bottle);
+  const [buy,setBuy]=useState(false);
+  const clickBuyBtn=()=>
+  {
+    setBuy(!buy);
+    handleBottle(bottle);
+
+
+
+  }
   return (
-    <div>
+  
      
 
-      <div className='card '>
-        <h3>Bottle : {bottle.bottle.id}</h3>
-        <img className="bottle-img"src={bottle.bottle.image} alt="" />
+      <div className={`card && ${buy && 'buy-card'}`}>
+        <h3>Bottle : {bottle.id}</h3>
+        <h4>Bottle Name : {bottle.name}</h4>
+        <h4>Bottle Type : {bottle.type}</h4>
+        <img className="bottle-img"src={bottle.image} alt="" />
+        <h4>Bottle price : {bottle.price}</h4>
+        <button onClick={clickBuyBtn}>{buy? "Buy" : "Not Buy"}</button>
+        
 
       </div>
 
-    </div>
+  
 
   );
 };
--- src/component/bottles/Bottles.jsx
+++ src/component/bottles/Bottles.jsx
@@ -1,18 +1,29 @@
-import React, { use } from 'react';
-import Bottle from '../bottle/bottle';
+import React, { use, useState } from 'react';
+import Bottle from '../bottle/Bottle';
 import './bottles.css';
 const Bottles = ({bottlePromise}) => {
   // console.log(bottlePromise);
   const bottles=use(bottlePromise);
   // console.log(bottles);
+  const [selectedbuy,setSelected]=useState([]);
+  const handleBottle=(bottle)=>
+  {
+    const newSelected=[...selectedbuy,bottle];
+
+    setSelected(newSelected);
+    // console.log(selected);
+    
+  }
   return (
     <div className=''>
+       
        <h2>Bottle Show </h2>
+       <h2>Number of bottle select : {selectedbuy.length} </h2>
        <div className='grid-contrainer'>
        {
         
 
-        bottles.map(bottle => <Bottle bottle={bottle}></Bottle>)
+        bottles.map(bottle => <Bottle key={bottle.id}  handleBottle={ ()=>handleBottle(bottle)} bottle={bottle}></Bottle>)
       }
 
 
